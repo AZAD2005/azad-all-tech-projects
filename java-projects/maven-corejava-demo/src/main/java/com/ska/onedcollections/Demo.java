65f3b952e8983de3507144da037b5388f93a7e5b
azadsk <skazad2967@gmail.com>
2026-07-09 06:36:26 +0530

    new

--- java-projects/maven-corejava-demo/src/main/java/com/ska/onedcollections/Demo.java
+++ java-projects/maven-corejava-demo/src/main/java/com/ska/onedcollections/Demo.java
@@ -1,4 +1,21 @@
 package com.ska.onedcollections;
 
+import java.util.ArrayList;
+
 public class Demo {
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("azad");
+        arrayList.add("azad");
+        arrayList.add("azad");
+        arrayList.add("kamal");
+        arrayList.add("azad");
+        arrayList.add("azad");
+        arrayList.trimToSize();
+
+        for (Object o : arrayList) {
+            System.out.println(o);
+        }
+    }
 }
